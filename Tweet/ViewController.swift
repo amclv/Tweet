f52f241e4832a16127a8004152e7c1ae332779c6
Aaron <toscleveland@gmail.com>
2020-08-29 19:57:13 -0500

    Init Project

---
+++ Tweet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tweet
+//
+//  Created by Aaron Cleveland on 8/29/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
