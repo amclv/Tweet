4da1a5d3fef607c36bf60f4eb0511315d683931d
Aaron <toscleveland@gmail.com>
2020-08-29 22:44:05 -0500

    Added TabbarController

---
+++ Tweet/Controller/FeedController.swift
@@ -0,0 +1,11 @@
+//
+//  FeedController.swift
+//  Tweet
+//
+//  Created by Aaron Cleveland on 8/29/20.
+//
+
+import Foundation
+import UIKit
+
+
---
+++ Tweet/Controller/MainTabController.swift
@@ -0,0 +1,24 @@
+//
+//  MainTabController.swift
+//  Tweet
+//
+//  Created by Aaron Cleveland on 8/29/20.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    // MARK: - Properties
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+    
+    // MARK: - Helpers
+    func configureViewControllers() {
+        
+    }
+}
